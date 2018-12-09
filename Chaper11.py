4273753bc85e89d4ceb56c43f268a81f2c42b6e7
Quoc Bao <levuquocbao.env@gmail.com>
2018-12-09 16:30:25 +0700

    Update

--- Chaper11.py
+++ Chaper11.py
@@ -36,9 +36,9 @@ def keys(h):
     for text in h:
         outstring.append(text)
     return outstring
-h=histogram_char('carrot')
-print_his(h)
-print(keys(h))
+# h=histogram_char('carrot')
+# print_his(h)
+# print(keys(h))
 # 11.4
 def reverse_lookup(h,n):
     listout=[]
@@ -46,4 +46,47 @@ def reverse_lookup(h,n):
         if h[i]==n:
             listout.append(i)
     return listout
-print(reverse_lookup(h,1))
+# print(reverse_lookup(h,1))
+def reversed_dict(h):
+    invers=dict()
+    for key in h:
+        val =h[key]
+        if val not in invers:
+            invers[val]=[key]
+        else:
+            invers[val].append(key)
+    return invers
+# print(reversed_dict(h))
+def reversed_dict_setdef(h):
+    print(h)
+    invers=dict()
+    for key in h:
+        print('key', '-', 'val')
+        val =h[key]
+        print(key,'-',val)
+        invers.setdefault(val, list()).append(key)
+    return invers
+# print(reversed_dict_setdef(h))
+# 11.6 Fibonacci compare
+def fibonaccy_recursion(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fibonaccy_recursion(n-1)+fibonaccy_recursion(n-2)
+def fibonaccy_memo(n):
+    '''using memo technique to reduce calculation time for fibonacci number'''
+    # print(knownF)
+    if n in knownF:
+        return knownF[n]
+    else:
+        newF=fibonaccy_memo(n-1)+fibonaccy_memo(n-2)
+        knownF[n]=newF
+    return newF
+starttime=time.time()
+print(fibonaccy_recursion(30))
+print('used time',time.time()-starttime)
+starttime=time.time()
+knownF={0:0,1:1}
+print(fibonaccy_memo(100))
+print('used time',time.time()-starttime)
